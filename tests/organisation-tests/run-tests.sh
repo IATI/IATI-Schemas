9574f3ef157abad1be186f1eec1a98d9d82181e4
David Megginson <david.megginson@gmail.com>
2010-11-07 19:10:58 +0000

    Start file renaming from organization to organisation.

---
+++ tests/organisation-tests/run-tests.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+########################################################################
+# Organization schema tests.
+########################################################################
+
+SCHEMA=../../iati-organizations-schema.xsd
+
+function fail {
+    echo -n '['
+    tput setf 4
+    echo -n FAIL
+    tput setf 0
+    echo -n '] '
+    echo $1
+}
+
+function pass {
+    echo -n '['
+    tput setf 2
+    echo -n PASS
+    tput setf 0
+    echo -n '] '
+    echo $1
+}
+
+for f in should-fail/*.xml; do
+    xmllint -noout --schema $SCHEMA $f >/dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        fail $f
+    else
+        pass $f
+    fi
+done
+
+for f in should-pass/*.xml; do
+    xmllint -noout --schema $SCHEMA $f >/dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        pass $f
+    else
+        fail $f
+    fi
+done
