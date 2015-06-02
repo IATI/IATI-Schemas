b6123303237096528e0a9041ce6490b598abf251
Herb Caudill <herb@caudillweb.com>
2015-06-02 17:35:34 -0400

    tabs -> spaces

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -4,10 +4,10 @@
 ########################################################################
 
 declare -A SCHEMAS=( 
-	# schema name => test folder 
-	["activities"]="activity-tests" 
-	["organisations"]="organisation-tests" 
-	["registry-record"]="registry-tests" 
+    # schema name => test folder 
+    ["activities"]="activity-tests" 
+    ["organisations"]="organisation-tests" 
+    ["registry-record"]="registry-tests" 
 )
 
 COLOR_BG=0 # Black
@@ -21,51 +21,51 @@ count_total=0
 exitcode=0
 
 function fail {
-	((count_failed++))
+    ((count_failed++))
     tput setaf $COLOR_FAIL
     echo '[FAIL]' $1
     tput setaf $COLOR_FG
 }
 
 function pass {
-	((count_passed++))
+    ((count_passed++))
     tput setaf $COLOR_PASS
     echo '[PASS]' $1
     tput setaf $COLOR_FG
 }
 
 function run_tests {
-	local validation_type=$1 # should-pass or should-fail
-	local xsd=$2
-	local dir=$3
-	for f in $(find "$dir/$validation_type" -name '*.xml' | sort); do
-		((count_total++))
-	    xmllint -noout --schema $xsd $f >/dev/null 2>&1
-	    local valid=$?
-	    local test_passed
-	    if [ $validation_type == "should-pass" ]; then
-	    	test_passed=$[$valid==0]
-    	else 
-	    	test_passed=$[$valid>0]
-    	fi
-	    if [ $test_passed == 0 ]; then
-	        fail $f
-	        exitcode=1
-	    else
-	        pass $f
-	    fi
-	done
+    local validation_type=$1 # should-pass or should-fail
+    local xsd=$2
+    local dir=$3
+    for f in $(find "$dir/$validation_type" -name '*.xml' | sort); do
+        ((count_total++))
+        xmllint -noout --schema $xsd $f >/dev/null 2>&1
+        local valid=$?
+        local test_passed
+        if [ $validation_type == "should-pass" ]; then
+            test_passed=$[$valid==0]
+        else 
+            test_passed=$[$valid>0]
+        fi
+        if [ $test_passed == 0 ]; then
+            fail $f
+            exitcode=1
+        else
+            pass $f
+        fi
+    done
 }
 
 tput setab $COLOR_BG
 tput setaf $COLOR_FG
 
 for s in "${!SCHEMAS[@]}"; do
-	dir=${SCHEMAS[$s]}
-	xsd="../iati-$s-schema.xsd"
-	echo "Running $s tests"
-	run_tests "should-pass" $xsd $dir
-	run_tests "should-fail" $xsd $dir
+    dir=${SCHEMAS[$s]}
+    xsd="../iati-$s-schema.xsd"
+    echo "Running $s tests"
+    run_tests "should-pass" $xsd $dir
+    run_tests "should-fail" $xsd $dir
     echo
 done
 
