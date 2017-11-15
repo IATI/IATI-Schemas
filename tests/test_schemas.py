1388d4deb8eb157e0d891fc9d6210fe0af924a7a
dalepotter <dalepotter@gmail.com>
2017-11-15 12:11:31 +0000

    Add tests for organisation schema test cases
    This adds the `iati-organisations-schema.xsd` file to the testrunner.
    
    Code requires refactoring to make more DRY.

--- tests/test_schemas.py
+++ tests/test_schemas.py
@@ -20,6 +20,20 @@ def activity_schema():
     return schema
 
 
+@pytest.fixture(scope='module')
+def organisation_schema():
+    """Return the v2.03 IATI Organisation Schema.
+
+    Todo:
+        Replace current monkeypatch of the default v2.02 schema with a call to `iati.schemas.ActivitySchema('iati-activities-schema.xsd')`
+        Requires resolution of issue: https://github.com/IATI/pyIATI/issues/234
+
+    """
+    schema = iati.default.organisation_schema(version='2.02')
+    schema._schema_base_tree = lxml.etree.parse('iati-organisations-schema.xsd')
+    return schema
+
+
 def list_xml_files_recursively(directory_path):
     """Return the full filepaths of all XML files within the input directory_path folder.
 
@@ -55,13 +69,24 @@ def load_as_dataset(filepath):
 
 
 @pytest.mark.parametrize('filepath', list_xml_files_recursively('tests/activity-tests/should-pass/') +  # Legacy tests
-                         list_xml_files_recursively('tests/should-pass/'))  # Tests in new format
-def test_pass_files(activity_schema, filepath):
-    """Check that all 'should-pass' test files are XML and pass Schema validation."""
+                         list_xml_files_recursively('tests/should-pass/iati-activities/'))  # Tests in new format
+def test_activity_pass_files(activity_schema, filepath):
+    """Check that all activity schema 'should-pass' test files are XML and pass Schema validation."""
     dataset = load_as_dataset(filepath)
 
     assert iati.validator.is_xml(dataset)
     assert iati.validator.is_iati_xml(dataset, activity_schema)
+
+
+@pytest.mark.parametrize('filepath', list_xml_files_recursively('tests/organisation-tests/should-pass/') +  # Legacy tests
+                         list_xml_files_recursively('tests/should-pass/iati-organisations/'))  # Tests in new format
+def test_organisation_pass_files(organisation_schema, filepath):
+    """Check that all organisation schema 'should-pass' test files are XML and pass Schema validation."""
+    dataset = load_as_dataset(filepath)
+
+    assert iati.validator.is_xml(dataset)
+    assert iati.validator.is_iati_xml(dataset, organisation_schema)
+
 
 @pytest.mark.parametrize('filepath', list_xml_files_recursively('tests/activity-tests/should-fail/'))
 def test_fail_files(activity_schema, filepath):
@@ -72,9 +97,9 @@ def test_fail_files(activity_schema, filepath):
     assert not iati.validator.is_iati_xml(dataset, activity_schema)
 
 
-@pytest.mark.parametrize('filepath', list_xml_files_recursively('tests/should-fail/'))
-def test_2_03_fail_files(activity_schema, filepath):
-    """Check that all 'should-fail' test files are XML but fail IATI Schema validation for the expected reason.
+@pytest.mark.parametrize('filepath', list_xml_files_recursively('tests/should-fail/iati-activities/'))
+def test_2_03_activity_fail_files(activity_schema, filepath):
+    """Check that all activity 'should-fail' test files are XML but fail IATI Schema validation for the expected reason.
 
     The expected reason must be stored in the filename of the test case, according to the pyIATI error name. pyIATI error names can be found at: https://github.com/IATI/pyIATI/blob/master/iati/resources/lib_data/validation_err_codes.yaml
 
@@ -88,6 +113,27 @@ def test_2_03_fail_files(activity_schema, filepath):
     failure_reason = filename_no_extension.split("_")[0]
 
     error_log = iati.validator.validate_is_iati_xml(dataset, activity_schema)
+
+    assert iati.validator.is_xml(dataset)
+    assert error_log.contains_error_called(failure_reason)
+
+
+@pytest.mark.parametrize('filepath', list_xml_files_recursively('tests/should-fail/iati-organisations/'))
+def test_2_03_organisation_fail_files(organisation_schema, filepath):
+    """Check that all organisation 'should-fail' test files are XML but fail IATI Schema validation for the expected reason.
+
+    The expected reason must be stored in the filename of the test case, according to the pyIATI error name. pyIATI error names can be found at: https://github.com/IATI/pyIATI/blob/master/iati/resources/lib_data/validation_err_codes.yaml
+
+    This error name must be inserted in the filename before the first underscore character. Example filename: err-not-iati-xml-missing-attribute_all-required-attributes-missing.xml
+
+    """
+    dataset = load_as_dataset(filepath)
+
+    filename = os.path.split(filepath)[-1]
+    filename_no_extension = filename.split('.')[0]
+    failure_reason = filename_no_extension.split("_")[0]
+
+    error_log = iati.validator.validate_is_iati_xml(dataset, organisation_schema)
 
     assert iati.validator.is_xml(dataset)
     assert error_log.contains_error_called(failure_reason)
